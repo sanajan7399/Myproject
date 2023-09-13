c207cb6d8c15efb5f8331dd1bf617a7e3a835e05
user <user@DESKTOP-ISDQQNT>
2023-09-13 11:16:42 +0530

    project1

---
+++ myproject/src/inheritance1/Dog.java
@@ -0,0 +1,8 @@
+package inheritance1;
+
+public class Dog {
+public void run()
+{
+System.out.println("running");
+}
+}
---
+++ myproject/src/mypack1/Babydog.java
@@ -0,0 +1,14 @@
+package mypack1;
+
+public class Babydog extends maven {
+public void jump()
+{
+	System.out.println("Animal");
+}
+public static void main(String args[])
+{
+	Babydog obj = new Babydog();
+	obj.jump();
+	obj.run();
+}
+}
